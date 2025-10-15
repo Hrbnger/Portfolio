dfa2a33fec37dd15ff03b9029a044f7813251eb2
Hrbnger <kutovictor17@gmail.com>
2025-10-15 10:17:57 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,6 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({ 
   base: '/Portfolio/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
